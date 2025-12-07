396e56d06fe62c3d50e24f5221ae64804a15477c
kontex148 <kontex148@gmail.com>
2025-12-07 13:46:06 +0200

    Implemented public key request to server

--- lab3/client.py
+++ lab3/client.py
@@ -8,7 +8,7 @@ from socket import *
 from cryptography.hazmat.primitives.asymmetric import rsa
 import logging
 
-from lab3.common import TransferDTO, ActionMode, encode_public_key, decode_public_key
+from lab3.common import TransferDTO, ActionMode, encode_public_key, decode_public_key, ServerResponseDTO
 
 logging.basicConfig(format='[%(threadName)s] %(asctime)s %(message)s', level=logging.INFO)
 logger = logging.getLogger()
@@ -36,32 +36,42 @@ class Client:
     def __del__(self):
         self.client_socket.close()
 
+    def send_dto(self, dto: TransferDTO):
+        payload = dto.to_bytes()
+        length = len(payload).to_bytes(4, 'big')
+        self.client_socket.sendall(length + payload)
+
     def generate_and_register_key(self):
         logger.info('Generating RSA key pair...')
         self.private_key, self.public_key = generate_rsa_key_pair()
         logger.info('RSA key pair generated!')
+        logger.info(f'Public key: {self.public_key}')
 
         data = f"{self.client_id}\n".encode() + encode_public_key(self.public_key)
         dto = TransferDTO(action=ActionMode.REGISTER_PUBLIC_KEY, data=data)
 
         logger.info('Sending DTO to server...')
-        self.client_socket.sendall(dto.to_bytes())
+        self.send_dto(dto)
 
         response = self.client_socket.recv(4096)
-        logger.info(f'From Server: {response!r}')
+        response = ServerResponseDTO.from_bytes(response)
+        if response.success:
+            logger.info('RSA key pair registered successfully!')
+        else:
+            logger.error('Error registering RSA key pair!')
 
     def request_public_key(self, key_id: int):
         data = key_id.to_bytes(4, byteorder='big')
         dto = TransferDTO(action=ActionMode.REQUEST_PUBLIC_KEY, data=data)
-        self.client_socket.sendall(dto.to_bytes())
+        self.send_dto(dto)
 
         response = self.client_socket.recv(4096)
-        requested_public_key = decode_public_key(response)
-        print(
-            f"Public key for user {key_id}: {requested_public_key}"
-        )
-        logger.info(f'From Server: {response!r}')
-
+        response = ServerResponseDTO.from_bytes(response)
+        if response.success:
+            public_key = decode_public_key(response.data)
+            logger.info(f'Public key for user {key_id}: {public_key}')
+        else:
+            logger.error(f'Error requesting public key for user {key_id} : {response.data.decode()}')
 
 def print_help():
     print("=" * 40)
@@ -87,11 +97,12 @@ def run_communication():
     while to_loop:
         command = input("Enter command (help for list of commands): ")
         match command:
-            case 1:
+            case "1":
                 client.generate_and_register_key()
-            case 2:
-
-            case 3:
+            case "2":
+                requested_id = input("Enter user ID to request public key for: ")
+                client.request_public_key(int(requested_id))
+            case "3":
                 to_loop = False
             case _:
                 print_help()
--- lab3/key_server.py
+++ lab3/key_server.py
@@ -8,7 +8,7 @@ from socket import *
 from threading import Thread
 import logging
 
-from lab3.common import TransferDTO, ActionMode, encode_public_key, decode_public_key
+from lab3.common import TransferDTO, ActionMode, encode_public_key, decode_public_key, ServerResponseDTO
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -35,30 +35,53 @@ def receive_all_bytes(sock):
         return b'Connection: timeout'
     return data
 
+def recv_exact(sock, n):
+    data = b''
+    while len(data) < n:
+        chunk = sock.recv(n - len(data))
+        if not chunk:
+            raise ConnectionError("Socket closed")
+        data += chunk
+    return data
+
+
+def recv_dto(sock):
+    # read 4-byte length
+    raw_len = recv_exact(sock, 4)
+    msg_len = int.from_bytes(raw_len, 'big')
+    # read actual payload
+    msg = recv_exact(sock, msg_len)
+    return msg
+
 def register_public_key(dto: TransferDTO, cs: socket):
-    raw = dto.data.decode()
-
-    user_id_str, pem_key = raw.split('\n', 1)
-    user_id = int(user_id_str)
-
-    user_public_key = decode_public_key(pem_key.encode())
-    public_keys[user_id] = user_public_key
-
-    logger.info(f"Public key registered: user {user_id}")
-    cs.sendall(b'OK')
+    try:
+        raw = dto.data
+        user_id_data, pem_key = raw.split(b'\n', 1)
+        user_id = int(user_id_data.decode())
+        user_public_key = decode_public_key(pem_key)
+        public_keys[user_id] = user_public_key
+        logger.info(f"Public key registered: user {user_id}")
+        response_dto = ServerResponseDTO(True, b'OK')
+    except Exception as e:
+        logger.error(f'Error registering public key: {e}')
+        response_dto = ServerResponseDTO(False, b'Error registering public key')
+    cs.sendall(response_dto.to_bytes())
 
 def request_public_key(dto: TransferDTO, cs: socket):
-    user_id = int(dto.data)
+    user_id = int.from_bytes(dto.data, 'big')
     public_key = public_keys.get(user_id)
     if public_key:
-        cs.sendall(encode_public_key(public_key))
+        response_dto = ServerResponseDTO(True, encode_public_key(public_key))
+    else:
+        response_dto = ServerResponseDTO(False, b'Could not find public key for user_id ' + str(user_id).encode())
+    cs.sendall(response_dto.to_bytes())
 
 def client_handler(cs: socket):
-    cs.settimeout(5)
+    #cs.settimeout(5)
     logger.info('Serving client...')
     toLoop = True
     while toLoop:
-        request = receive_all_bytes(cs)
+        request = recv_dto(cs)
 
         if not request:
             logger.warning('Connection interrupted')
